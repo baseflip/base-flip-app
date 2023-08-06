efa7f2bd179e3d49319d1dad5e1d2284c83fa853
Lenny <dappdeployer@gmail.com>
2023-08-07 01:52:18 +0200

    Add networks

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,11 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   namedAccounts: {
     deployer: 0
+  },
+  networks: {
+    base_goerli: {
+      url: "https://goerli.base.org",
+      accounts: [process.env.PRIVATE_KEY ?? ""]
+    },
+    base_mainnet: {
+      url: "https://mainnet.base.org",
+      accounts: [process.env.PRIVATE_KEY ?? ""]
+    },
   }
 };
 
