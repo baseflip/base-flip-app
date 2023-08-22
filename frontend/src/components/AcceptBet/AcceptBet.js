826da1ce31a6f21b47efe2e99f91e89b64bff0d6
Lenny <dappdeployer@gmail.com>
2023-08-23 00:06:13 +0200

    Add timer

--- frontend/src/components/AcceptBet/AcceptBet.js
+++ frontend/src/components/AcceptBet/AcceptBet.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { ethers, JsonRpcProvider, Contract } from 'ethers';
 import abiData from '../../abi.json';
+import EthereumContext from '../../EthereumContext';
 
 const CONTRACT_ADDRESS = "0x70751cF31d8f31d6622760D243F5E4e150efb20b";
 const RPC_URL = "https://goerli.base.org";
 
 function AcceptBet() {
+  const { signer } = useContext(EthereumContext);
   const [gameDetails, setGameDetails] = useState(null);
+  const [betExpired, setBetExpired] = useState(false);
 
   // Create a provider instance to connect to a custom Ethereum node
   const provider = useMemo(() => {
@@ -24,6 +27,14 @@ function AcceptBet() {
   useEffect(() => {
     const fetchGameDetails = async () => {
       const game = await contractInstance.games(gameId);
+      const startTime = Number(game.startTime);
+
+      // Check if the current time is more than 15 minutes past the start time
+      if (Date.now() / 1000 - startTime > 15 * 60) {
+        setBetExpired(true);
+        return;
+      }
+
       const betAmountInEther = ethers.formatEther(game.betAmount);
       setGameDetails({
         player1: game.player1,
@@ -40,7 +51,9 @@ function AcceptBet() {
 
   return (
     <div>
-      {gameDetails ? (
+      {betExpired ? (
+        <p>The bet has expired.</p>
+      ) : gameDetails ? (
         <>
           <h2>Accept Bet</h2>
           <p>Bet Amount: {gameDetails.betAmount} ETH</p>
