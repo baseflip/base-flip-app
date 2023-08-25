7071b4825c62a1b1c91ee19c869e5679090a4afe
Lenny <dappdeployer@gmail.com>
2023-08-26 01:10:55 +0200

    Move share logic to room

--- frontend/src/components/AcceptBet/AcceptBet.js
+++ frontend/src/components/AcceptBet/AcceptBet.js
@@ -13,6 +13,8 @@ function AcceptBet() {
   const [gameDetails, setGameDetails] = useState(null);
   const [betExpired, setBetExpired] = useState(false);
   const { gameId } = useParams();
+  const [withdrawn, setWithdrawn] = useState(false);
+  const [generatedLink, setGeneratedLink] = useState('');
   const navigate = useNavigate();
 
   // Create a provider instance to connect to a custom Ethereum node
@@ -23,7 +25,7 @@ function AcceptBet() {
   }, []);
   
   // Create a contract instance using the public provider
-  const contractInstance = useMemo(() => {
+  const contractInstanceProvider = useMemo(() => {
     return new Contract(CONTRACT_ADDRESS, abiData.abi, provider);
   }, [provider]);
 
@@ -42,23 +44,17 @@ function AcceptBet() {
       }
     };
   
-    contractInstance.on("CoinFlipped", onCoinFlipped);
+    contractInstanceProvider.on("CoinFlipped", onCoinFlipped);
     // return () => {
     //   contractInstanceProvider.off("CoinFlipped", onCoinFlipped);
     // };
-  }, [contractInstance, navigate, gameId]);
+  }, [contractInstanceProvider, navigate, gameId]);
 
 
   useEffect(() => {
     const fetchGameDetails = async () => {
-      const game = await contractInstance.games(gameId);
+      const game = await contractInstanceProvider.games(gameId);
       const startTime = Number(game.startTime);
-
-      // Check if the current time is more than 15 minutes past the start time
-      if (Date.now() / 1000 - startTime > 15 * 60) {
-        setBetExpired(true);
-        return;
-      }
 
       const betAmountInEther = ethers.formatEther(game.betAmount);
       setGameDetails({
@@ -66,10 +62,35 @@ function AcceptBet() {
         betAmount: betAmountInEther,
         betAmountRaw: game.betAmount
       });
+
+      // Check if the current time is more than 15 minutes past the start time
+      if (Date.now() / 1000 - startTime > 15 * 60) {
+        setBetExpired(true);
+        return;
+      }
+
     };
 
     fetchGameDetails();
-  }, [gameId, contractInstance]);
+  }, [gameId, contractInstanceProvider]);
+
+  useEffect(() => {
+    const link = "http://localhost:3000/room/" + gameId.toString();
+    setGeneratedLink(link);
+  }, [gameId]);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setBetExpired(true);
+    }, 900000);
+  
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, []);
+  
 
   const handleJoinGame = async () => {
     try {
@@ -82,22 +103,64 @@ function AcceptBet() {
     }
   };
 
+  const handleWithdraw = async () => {
+    try {
+      const tx1 = await contractInstanceSigner.checkGameTimeout(gameId);
+      await tx1.wait();  // Wait for the transaction to be confirmed
+
+      // Then proceed with the withdrawal
+      const tx2 = await contractInstanceSigner.withdraw();
+      await tx2.wait();
+      setWithdrawn(true);
+    } catch (error) {
+      console.error("Error withdrawing:", error);
+    }
+  };
+
   return (
     <div>
-      {betExpired ? (
-        <p>The bet has expired.</p>
-      ) : gameDetails && gameId ? (
+      {gameDetails && gameId ? (
         <>
-          <h2>Accept Bet</h2>
+          <h2>Game Details</h2>
           <p>Bet Amount: {gameDetails.betAmount} ETH</p>
           <p>Player 1 Address: {gameDetails.player1}</p>
-          {account ? (
+  
+          {gameDetails.player1.toLowerCase() === account ? (
+            <>
+            <h2>Share the room link with someone</h2>
+            
+            <div className="link-section">
+              <input type="text" readOnly value={generatedLink} />
+            </div>
+  
+            <div className="bet-output-section">
+              <div className="withdraw-section">
+                <button
+                  onClick={handleWithdraw}
+                  disabled={!betExpired || withdrawn}
+                  style={{
+                    backgroundColor: betExpired && !withdrawn ? '#4CAF50' : 'gray'
+                  }}
+                >
+                  Request Withdraw
+                </button>
+                {!withdrawn && (
+                  <div className="tooltip-withdraw">
+                    You can withdraw only once the bet has expired without the other user accepting, which takes 15 minutes.
+                  </div>
+                )}
+              </div>
+            </div>
+          </>
+          ) : (betExpired ? (
+            <p>The bet has expired.</p>
+          ) : account ? (
             <button onClick={handleJoinGame}>Accept</button>
           ) : (
             <button className="placeholder-button" onClick={connectWallet}>
               Connect Wallet
             </button>
-          )}
+          ))}
         </>
       ) : (
         <p>Loading game details...</p>
--- frontend/src/components/Bet/Bet.js
+++ frontend/src/components/Bet/Bet.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext, useEffect, useMemo } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Contract, WebSocketProvider } from 'ethers';
+import { Contract } from 'ethers';
 import { parseEther } from 'ethers/utils';
 import abiData from '../../abi.json';
 import EthereumContext from '../../EthereumContext';
@@ -10,46 +10,11 @@ const CONTRACT_ADDRESS = "0x70751cF31d8f31d6622760D243F5E4e150efb20b";
 
 function Bet() {
   const [betAmount, setBetAmount] = useState('');
-  const [generatedLink, setGeneratedLink] = useState('');
-  const [canWithdraw, setCanWithdraw] = useState(false);
-  const [withdrawTimeout, setWithdrawTimeout] = useState(null);
   const [transactionStatus, setTransactionStatus] = useState('');
-  const [gameId, setGameId] = useState(null);
-  const [withdrawn, setWithdrawn] = useState(false);
   const { signer } = useContext(EthereumContext);
   const navigate = useNavigate();
-
-  // Create a provider instance to connect to a custom Ethereum node
-  const provider = useMemo(() => {
-    return new WebSocketProvider(
-    `wss://delicate-crimson-dew.base-goerli.discover.quiknode.pro/3e739828ff34548682516310c83e2dbbb604b2b8/`
-  );
-  }, []);
-  
-  // Create a contract instance using the public provider
-  const contractInstanceProvider = useMemo(() => {
-    return new Contract(CONTRACT_ADDRESS, abiData.abi, provider);
-  }, [provider]);
-
   const contractInstance = new Contract(CONTRACT_ADDRESS, abiData.abi, signer);
   
-  useEffect(() => {
-    // Listen to the "CoinFlipped" event
-    const onCoinFlipped = (gameIdEvent, winner) => {
-      console.log("hi from Bet.js");
-      // Redirect to the "gameResult" component with the winner information
-      if(gameIdEvent === gameId) {
-        navigate('/game-result/' + gameId, { state: { winner, gameId } });
-      }
-    };
-  
-    contractInstanceProvider.on("CoinFlipped", onCoinFlipped);
-    // return () => {
-    //   contractInstanceProvider.off("CoinFlipped", onCoinFlipped);
-    // };
-  }, [contractInstanceProvider, navigate, gameId]);
-  
-
   const handleDeposit = async () => {
     try {
       setTransactionStatus('pending');
@@ -58,57 +23,23 @@ function Bet() {
       const tx = await contractInstance.startGame(amount, { value: amount });
       const receipt = await tx.wait();
 
+      
       if (receipt.logs && receipt.logs.length > 0) {
-          const parsedLog = contractInstance.interface.parseLog(receipt.logs[0]);
-          const gameId = parsedLog.args[0];
-          const link = "http://yourapp.com/room/" + gameId.toString();
-          setGeneratedLink(link);
-          setGameId(gameId);
+        const parsedLog = contractInstance.interface.parseLog(receipt.logs[0]);
+        const gameId = parsedLog.args[0];
+        navigate('/room/' + gameId);
       } else {
-          console.error("Failed to retrieve gameId from the transaction receipt.");
+        console.error("Failed to retrieve gameId from the transaction receipt.");
       }
-
-      setTransactionStatus('confirmed');
-
-      const timeoutId = setTimeout(() => {
-        setCanWithdraw(true);
-      }, 900000);
-
-      setWithdrawTimeout(timeoutId);
-
+      
     } catch (error) {
         console.error("Error depositing:", error);
         setTransactionStatus('error');
     }
   };
-
-  const handleWithdraw = async () => {
-    try {
-      // First, check if the game has timed out and update the game state if necessary
-      await contractInstance.checkGameTimeout(gameId);
   
-      // Then proceed with the withdrawal
-      const tx = await contractInstance.withdraw();
-      await tx.wait();
-      setCanWithdraw(false);
-      setWithdrawn(true);
-    } catch (error) {
-      console.error("Error withdrawing:", error);
-    }
-  };
-
-  useEffect(() => {
-    return () => {
-      if (withdrawTimeout) {
-        clearTimeout(withdrawTimeout);
-      }
-    };
-  }, [withdrawTimeout]);
-
   return (
     <div className="bet-container">
-      {transactionStatus !== 'confirmed' ? (
-        <>
           <div className="heading-section">
             <h2>Create a new bet and share it</h2>
             <div className="info-icon">
@@ -127,38 +58,6 @@ function Bet() {
             <button onClick={handleDeposit}>Deposit</button>
           </div>
           {transactionStatus === 'pending' && <p>Awaiting transaction confirmation...</p>}
-        </>
-      ) : (
-        <>
-          <h2>Share the room link with someone</h2>
-          
-          <div className="link-section">
-            <input type="text" readOnly value={generatedLink} />
-          </div>
-
-          <div className="bet-output-section">
-            <p>
-              {withdrawn ? "Deposit withdrawn" : `You've deposited: ${betAmount} ETH`}
-            </p>
-            <div className="withdraw-section">
-              <button
-                onClick={handleWithdraw}
-                disabled={!canWithdraw || withdrawn} // Disable the button if already withdrawn
-                style={{
-                  backgroundColor: canWithdraw && !withdrawn ? '#4CAF50' : 'gray'
-                }}
-              >
-                Withdraw
-              </button>
-              {!canWithdraw && !withdrawn && (
-                <div className="tooltip-withdraw">
-                  You can withdraw only once the bet has expired without the other user accepting, which takes 15 minutes.
-                </div>
-              )}
-            </div>
-          </div>
-        </>
-      )}
     </div>
   );
 }
